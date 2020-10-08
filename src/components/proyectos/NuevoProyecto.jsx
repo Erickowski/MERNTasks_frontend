462cd75e0f3b363048de08d0ca895bf3ddf65cf9
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-08 13:57:16 -0500

    Funcionalidad Nuevo Proyecto

--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NuevoProyecto = () => {
+  // State para proyecto
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
+  });
+  // extraer nombre del proyecto
+  const { nombre } = proyecto;
+  // Leer el contenido del input
+  const onChangeProyecto = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //  Cuando el usuario envia un proyecto
+  const onSubmitProyecto = (e) => {
+    e.preventDefault();
+    // Validar el proyecto
+
+    // Agregar al state
+
+    // Reiniciar el form
+  };
   return (
     <>
       <button className="btn btn-block btn-primario" type="button">
         Nuevo Proyecto
       </button>
-      <form action="" className="formulario-nuevo-proyecto">
+      <form
+        action=""
+        className="formulario-nuevo-proyecto"
+        onSubmit={onSubmitProyecto}
+      >
         <input
           type="text"
           name="nombre"
           id=""
           placeholder="Nombre proyecto"
           className="input-text"
+          onChange={onChangeProyecto}
+          value={nombre}
         />
         <input
           type="submit"
