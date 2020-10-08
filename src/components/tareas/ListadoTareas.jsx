b87cc7efbae1732dd2e36df1391b4486fb4765da
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-08 14:20:13 -0500

    Diseño Listado tareas y tarea

---
+++ src/components/tareas/ListadoTareas.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Tarea from "./Tarea";
+
+const ListadoTareas = () => {
+  const tareasProyecto = [
+    { nombre: "Elegir Plataforma", estado: true },
+    { nombre: "Elegir Colores", estado: false },
+    { nombre: "Elegir Plataformas de Pago", estado: false },
+    { nombre: "Elegir Hosting", estado: false },
+  ];
+  return (
+    <>
+      <h2>Proyecto: Tienda Virtual</h2>
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0 ? (
+          <li className="tarea">
+            <p>No hay Tareas</p>
+          </li>
+        ) : (
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default ListadoTareas;
