53f1f8315313f80c5b6a8a1f50bf012957570b46
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-08 22:02:25 -0500

    Ocultar formulario de tarea

--- src/components/tareas/FormTarea.jsx
+++ src/components/tareas/FormTarea.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+  // Extraer proyecto activo de state inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+  //  Si no hay proyecto seleccionado
+  if (!proyecto) return null;
+
+  // Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
   return (
     <div className="formulario">
       <form action="">
