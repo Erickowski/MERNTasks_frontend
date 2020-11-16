107292c00ae72f60e8f516df162b446c7ec00727
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-16 08:42:36 -0600

    Pruebas para formulario de nueva cuenta

--- cypress/integration/Formulario.spec.js
+++ cypress/integration/Formulario.spec.js
@@ -29,4 +29,34 @@ describe("<Formulario />", () => {
 
     cy.visit("/nueva-cuenta");
   });
+
+  it("<NuevaCuenta /> - Verificar componente de nueva cuenta", () => {
+    cy.get("[data-cy=titulo]")
+      .should("exist")
+      .invoke("text")
+      .should("equal", "Obtener una cuenta");
+
+    cy.get("[data-cy=nueva-cuenta]").should("exist");
+
+    cy.get("[data-cy=nombre-input]").should("exist");
+    cy.get("[data-cy=email-input]").should("exist");
+    cy.get("[data-cy=password-input]")
+      .should("exist")
+      .should("have.prop", "type")
+      .should("equal", "password");
+    cy.get("[data-cy=confirmar-input]").should("exist");
+
+    cy.get("[data-cy=submit-nueva-cuenta]")
+      .should("exist")
+      .should("have.class", "btn-primario")
+      .should("have.value", "Registrarme")
+      .should("not.have.value", "Registrarme ahora");
+
+    cy.get("[data-cy=enlace-login]")
+      .should("exist")
+      .should("have.attr", "href")
+      .should("eq", "/");
+
+    cy.visit("/");
+  });
 });
--- src/components/auth/NuevaCuenta.jsx
+++ src/components/auth/NuevaCuenta.jsx
@@ -79,11 +79,12 @@ const NuevaCuenta = (props) => {
         <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>
       )}
       <div className="contenedor-form sombra-dark">
-        <h1>Obtener una cuenta</h1>
-        <form onSubmit={iniciarSesion}>
+        <h1 data-cy="titulo">Obtener una cuenta</h1>
+        <form onSubmit={iniciarSesion} data-cy="nueva-cuenta">
           <div className="campo-form">
             <label htmlFor="nombre">Nombre</label>
             <input
+              data-cy="nombre-input"
               type="text"
               id="nombre"
               name="nombre"
@@ -95,6 +96,7 @@ const NuevaCuenta = (props) => {
           <div className="campo-form">
             <label htmlFor="email">Email</label>
             <input
+              data-cy="email-input"
               type="email"
               id="email"
               name="email"
@@ -106,6 +108,7 @@ const NuevaCuenta = (props) => {
           <div className="campo-form">
             <label htmlFor="password">Password</label>
             <input
+              data-cy="password-input"
               type="password"
               id="password"
               name="password"
@@ -117,6 +120,7 @@ const NuevaCuenta = (props) => {
           <div className="campo-form">
             <label htmlFor="confirmar">Confirmar Password</label>
             <input
+              data-cy="confirmar-input"
               type="password"
               id="confirmar"
               name="confirmar"
@@ -127,13 +131,14 @@ const NuevaCuenta = (props) => {
           </div>
           <div className="campo-form">
             <input
+              data-cy="submit-nueva-cuenta"
               type="submit"
               value="Registrarme"
               className="btn btn-primario btn-block"
             />
           </div>
         </form>
-        <Link to={"/"} className="enlace-cuenta">
+        <Link to={"/"} className="enlace-cuenta" data-cy="enlace-login">
           Iniciar Sesión
         </Link>
       </div>
